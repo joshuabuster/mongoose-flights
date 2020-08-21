c0c4f0bc498193a863ef95f85d8990ba8a8bff32
joshuabuster <63068851+joshuabuster@users.noreply.github.com>
2020-08-21 08:48:38 -0700

    adds create function and html for flights index view

--- controllers/flights.js
+++ controllers/flights.js
@@ -2,8 +2,17 @@ const Flight = require('../models/flight');
 
 module.exports = {
     new: newFlight,
-    index
+    index,
+    create
 };
+
+function create(req, res) {
+  const flight = new Flight(req.body);
+  flight.save(function(err) {
+    if(err) return res.render('flights/new');
+    res.redirect('/flights')
+  });
+}
 
 function newFlight(req,res) {
     res.render('flights/new');
--- routes/flights.js
+++ routes/flights.js
@@ -8,7 +8,7 @@ router.get('/', flightsCtrl.index);
 // GET /flights/new
 router.get('/new', flightsCtrl.new);
 // POST /flights
-// router.post('/', flightsCtrl.create);
+router.post('/', flightsCtrl.create);
 
 
 module.exports = router;
